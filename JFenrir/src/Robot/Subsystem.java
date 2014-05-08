c44061ed6ebb4d498971be5c234d625c00787f8b
TPackard <tyler.packard86@gmail.com>
2014-05-08 15:23:57 -0700

    Create Subsystem.java

---
+++ JFenrir/src/Robot/Subsystem.java
@@ -0,0 +1,27 @@
+/**
+ * Subsystems represent a subsytem of the robot such as the shooter or drivetrain.
+ * 
+ * @author Tyler Packard
+ */
+public abstract Subsystem {
+
+	/**
+	 * Initialize the subsystem.
+	 */
+	public void init() {}
+	
+	/**
+	 * Disable the subsystem.
+	 */
+	public void disable() {}
+	
+	/**
+	 * Update the subsystem.
+	 */
+	public void update() {}
+	
+	/**
+	 * Give the subsystem a command to run.
+	 */
+	public void runCommand(RobotCommand command) {}
+}

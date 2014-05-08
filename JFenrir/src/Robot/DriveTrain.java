34e3c251efd4b4b3e5b016ec0d196827305caa29
TPackard <tyler.packard86@gmail.com>
2014-05-08 14:36:43 -0700

    Added command example

--- JFenrir/src/Robot/DriveTrain.java
+++ JFenrir/src/Robot/DriveTrain.java
@@ -11,4 +11,15 @@ package Robot;
  */
 public class DriveTrain {
     
+    public static class SetSpeedCommand implements RobotCommand {
+        private int speed;
+        
+        public SetSpeedCommand(int speed) {
+            this.speed = speed;
+        }
+        
+        public void execute() {
+            // Set the speed of the victors to the variable speed
+        }
+    }
 }

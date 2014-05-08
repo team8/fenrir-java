960060c3939b946b09effa3ee08774c4ed104427
TPackard <tyler.packard86@gmail.com>
2014-05-08 14:22:02 -0700

    Optimized for Java
    Don't hate, please wait until the rest is done.

--- JFenrir/src/Robot/RobotCommand.java
+++ JFenrir/src/Robot/RobotCommand.java
@@ -1,125 +1,14 @@
-/*
- * See JFenrir.java for documentation
- * Coded by the 2013-14 Paly Robotics Programming Team
- */
-
 package Robot;
 
 /**
- * Runs commands to the robot
- * @author Paly Robotics
+ * The framework for a command that can be executed.
+ * 
+ * @author Tyler Packard
  */
-public class RobotCommand {
-    // Arguments used for Drivetrain methods
-//    class DriveArgs {
-//    }
-//
-//    class DriveDist extends DriveArgs {
-//        double driveDist;
-//    }
-//
-//    class RotAngle extends DriveArgs {
-//        double rotAngle;
-//    }
-//
-//    class RotSpeed extends DriveArgs {
-//        double rotSpeed;
-//    }
-//
-//    class DriveSpeed extends DriveArgs {
-//        double driveSpeed;
-//    }
-//
-//    class SpeedValue extends DriveArgs {
-//        double speedValue;
-//    }
-    class DriveArgs {
-        double driveDist;
-        double rotAngle;
-        double rotSpeed;
-        double driveSpeed;
-        double speedValue;
-    }
-
-    class ShooterArgs {
-        boolean trigger;
-    }
-
-    class RangefinderArgs {
-        float target;
-    }
-
-    public enum SubsystemType {
-        DRIVE,
-        SHOOTER,
-        ACCUMULATOR,
-        RANGEFINDER,
-        ROBOT;
-    }
-
-	/*SUBSYSTEM METHODS*/
-    public enum DriveMethod {
-        SETSPEED,
-        DRIVEDIST,
-        ROTATEANGLE,
-        ROTATESPEED,
-        STOPVICTORS;
-    }
-
-    public enum AccumulatorMethod {
-        ACCUMULATE,
-        STOP,
-        PASS;
-    }
-
-    public enum ExtensionMethod {
-        FOWARD,
-        BACKWARD,
-        HALT;
-    }
-
-    public enum ShooterMethod {
-        FIRE,
-        EJECT,
-        IDLE,
-        FLUSH,
-        MANUAL_FIRE,
-        MANUAL_LOAD;
-    }
-
-    public enum RangefinderMethod {
-        ANGLE,
-        SET_DIST,
-        LEFT_ON,
-        RIGHT_ON,
-        WALL_DIST;
-    }
-
-    public enum RobotMethod {
-        ALIGN_THEN_SHOOT;
-    }
-
-    Object arg;
-    private SubsystemType subsystem;
-    private Method method;
-
-    public RobotCommand(SubsystemType subsystemType, Method methodType, Object args) {
-        subsystem = subsystemType;
-        method = methodType;
-        arg = args;
-    }
-
-    public SubsystemType getSubsystem() {
-        return subsystem;
-    }
-
-    public Method getMethod() {
-        return method;
-    }
-}
-
-class Method {
-    RobotCommand.DriveMethod driveMethod;
-    RobotCommand.ShooterMethod shooterMethod;
-    RobotCommand.AccumulatorMethod accumulatorMethod;
+public interface RobotCommand {
+	
+	/**
+	 * Execute is a method that carries out a certain task that the command is supposed to do.
+	 */
+	void execute;
 }

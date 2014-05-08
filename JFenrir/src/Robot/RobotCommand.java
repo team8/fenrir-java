3b4c8e6ebd1fc60d293e52302f9aa4c39fc18b01
TPackard <tyler.packard86@gmail.com>
2014-05-08 15:11:14 -0700

    Added subsystems

--- JFenrir/src/Robot/RobotCommand.java
+++ JFenrir/src/Robot/RobotCommand.java
@@ -4,11 +4,25 @@ package Robot;
  * The framework for a command that can be executed.
  * 
  * @author Tyler Packard
+ * @author Joey Kellison-Linn
  */
-public interface RobotCommand {
+public abstract class RobotCommand {
+	
+	/**
+	 * The subsystem the robot class should send the command to.
+	 */
+	public final int SubsystemType;
+	
+	/**
+	 * Numbers that represent each sumbsystem.
+	 */
+	public static final int ACCUMULATOR = 0;
+	public static final int DRIVETRAIN = 1;
+	public static final int RANGEFINDER = 2;
+	public static final int SHOOTER = 3;
 	
 	/**
 	 * Execute is a method that carries out a certain task that the command is supposed to do.
 	 */
-	void execute();
+	abstract void execute();
 }

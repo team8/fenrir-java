55b4e45f275569d85a51eb03488e9bb9d1a8489e
TPackard <tyler.packard86@gmail.com>
2014-05-08 16:10:16 -0700

    Ported methods and variables

--- JFenrir/src/Robot/Rangefinder.java
+++ JFenrir/src/Robot/Rangefinder.java
@@ -5,10 +5,58 @@
 
 package Robot;
 
+import edu.wpi.first.wpilibj.*;
+
 /**
  * Runs the rangefinder
- * @author Paly Robotics
+ * @author Tyler Packard
  */
-public class Rangefinder {
+public class Rangefinder extends Subsystem {
+    private AnalogChannel ultraLeft = new AnalogChannel(Constants.ULTRASONIC_CHANNEL_LEFT);
+    private DigitalOutput rxLeft = new DigitalOutput(Constants.ULTRASONIC_RX_LEFT);
+    private AnalogChannel ultraRight = new AnalogChannel(Constants.ULTRASONIC_CHANNEL_RIGHT);
+    private DigitalOutput rxRight = new DigitalOutput(Constants.ULTRASONIC_RX_RIGHT);
     
+    private static final int IDLE = 0;
+    private static final int LEFT = 1;
+    private static final int RIGHT = 2;
+    private static final int FINISHED = 3;
+    private int state = IDLE;
+    
+    private double distInch = 0;
+    private double leftAvg = 0;
+	private double rightAvg = 0;
+	private int leftTotal = 0;
+	private int rightTotal = 0;
+	
+	
+	public double measureAngle() {
+	    // Probably obsolete
+	}
+	
+	public void rotateToWall() {
+	    // Probably obsolete
+	}
+	
+	public float wallDist() {
+	    // Probably obsolete
+	}
+	
+	public double getDist() {
+	    return distInch
+	}
+	
+	public void setDistToWall(float dist) {
+	    // Send a command to drivetrain
+	}
+	
+    @Override
+    public void runCommand(RobotCommand command) {
+        command.execute();
+    }
+    
+    @Override
+	public void update() {
+	    
+	}
 }

501e6455feb3c7b8eba002f876024b685e7c97b2
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2014-05-09 00:14:29 -0700

    ported code to java
    I did my Bio Contagion reflection :((((((((((((

--- JFenrir/src/Robot/JFenrir.java
+++ JFenrir/src/Robot/JFenrir.java
@@ -7,8 +7,69 @@ package Robot;
 
 /**
  * Main method?
- * @author Paly Robotics
+ * @author Neelay Junnarkar
  */
-public class JFenrir {
+public class JFenrir extends IterativeRobot {
+    
+    private Robot robot;
+    private HumanController humanController;
+    private AutonomousController autoController;
+    private Timer timer;
+    
+    public JFenrir(){
+        robot = new Robot();
+        humanController = new HumanController(robot);
+        autoController = new AutonomousControler(robot);
+        timer = new Timer();
+    }
+    
+    public void robotInit(){
+        robot.init();   
+    }
+    
+    public void autonomousInit(){
+        timer.start();
+	    robot.init();
+	    autoController.init();
+    }
+    
+    public void autonomousPeriodic(){
+        autoController.update();
+        robot.update();
+    }
+    
+    public void autonomousDisabled(){
+        
+    }
+    
+    public void disabledInit(){
+        robot.disable();   
+    }
+    
+    public void disabledPeriodic(){
+        robot.disable();
+        robot.update();
+    }
+    
+    public void teleopInit(){
+        robot.init();
+    }
+    
+    public void teleopPeriodic(){
+        humanController.update();
+        robot.update();
+    }
+    
+    public void teleopDisabled(){
+        
+    }
+    
+    public void testInit(){
+        
+    }
+    
+    public void testPeriodic(){
+        
+    }
     
 }

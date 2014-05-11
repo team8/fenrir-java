a9b5c0edeed67b7311db0d753ffebefdfad9cb73
Nihar Mitra <nihar.roy.mitra@gmail.com>
2014-05-10 18:31:24 -0700

    Use tabs not spaces, inserted space before {

--- JFenrir/src/Robot/Accumulator.java
+++ JFenrir/src/Robot/Accumulator.java
@@ -8,76 +8,75 @@ package Robot;
 import edu.wpi.first.wpilibj.*;
 
 /**
- * Runs the accumulator
+ * Runs the accumulator subsystem
  * @author 
  */
 public class Accumulator extends Subsystem {
+	
+	private Victor accuVic;
+	
+	private int state;
+	private static final int IDLE = 0, ACCUMULATING = 1, PASSING = 2; 
+	
+	public Accumulator() {
+		accuVic = new Victor(Constants.PORT_ACCUMULATOR_VIC_7);
+	}
+	
+	public void init() {
+		state = IDLE;
+	}
+	
+	public void disable() {
+		state = IDLE;
+	}
+	
+	public void update() {
+		switch(state) {
+		case IDLE:
+			setAllVics(0.0);
+			break;
+		case ACCUMULATING:
+			accuVic.set(-1.0);
+			break;
+		case PASSING:
+			accuVic.set(1.0);
+			break;
+		}
+	}
     
-    private Victor accuVic;
+	private void setAllVics(double spd) {
+		accuVic.set(spd);
+	}
+	
+	public static class AccumulateCommand extends RobotCommand {
+		subsystemType = ACCUMULATOR;
+		
+		void execute(Accumulator accu){
+			accu.state = ACCUMULATING;
+		}
+	}
     
-    private int state;
-    private static final int IDLE = 0, ACCUMULATING = 1, PASSING = 2; 
-    
-    public Accumulator(){
-        accuVic = new Victor(Constants.PORT_ACCUMULATOR_VIC_7);
+	public static class PassCommand extends RobotCommand {
+		subsystemType = ACCUMULATOR;
+		
+		void execute(Accumulator accu){
+			accu.state = PASSING;
+		}
     }
     
-    public void init(){
-        state = IDLE;
-    }
-    
-    public void disable(){
-        state = IDLE;
-    }
-    
-    public void update(){
-      switch(state){
-            case IDLE:
-                setAllVics(0.0);
-                break;
-            case ACCUMULATING:
-                accuVic.set(-1.0);
-                break;
-            case PASSING:
-                accuVic.set(1.0);
-                break;
-        }
-    }
-    
-    private void setAllVics(double spd){
-        accuVic.set(spd);
-    }
-    
-    public static class AccumulateCommand extends RobotCommand {
-    	subsystemType = ACCUMULATOR;
-        
-        void execute(Accumulator accu){
-            accu.state = ACCUMULATING;
-        }
-    }
-    
-    public static class PassCommand extends RobotCommand {
-        subsystemType = ACCUMULATOR;
-        
-        void execute(Accumulator accu){
-            accu.state = PASSING;
-        }
-    }
-    
-    public static class FlushCommand extends RobotCommand{
-        subsystemType = ACCUMULATOR;
-        
-        void execute(Accumulator accu){
-            accu.state = PASSING;
-        }
-    }
-    
-    public static class SetIdleCommand extends RobotCommand {
-        subsystemType = ACCUMULATOR;
-        
-        void execute(Accumulator accu){
-            accu.state = IDLE;
-        }
-    }
-    
+	public static class FlushCommand extends RobotCommand {
+		subsystemType = ACCUMULATOR;
+		
+		void execute(Accumulator accu){
+			accu.state = PASSING;
+		}
+	}
+	
+	public static class SetIdleCommand extends RobotCommand {
+		subsystemType = ACCUMULATOR;
+		
+		void execute(Accumulator accu){
+			accu.state = IDLE;
+		}
+	}
 }

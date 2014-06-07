fee214edc972434265f244a497707b168af9f27a
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2014-06-06 17:05:21 -0700

    changed AccumulateCommand to allow variable accumulating speeds

--- JFenrir/src/com/palyrobotics/Accumulator.java
+++ JFenrir/src/com/palyrobotics/Accumulator.java
@@ -13,95 +13,104 @@ import edu.wpi.first.wpilibj.*;
  * @author Neelay Junnarkar
  */
 public class Accumulator extends Subsystem {
-	
-	private Victor accuVic;
-	
-	private int state;
-	
-	public void setState(int state) { 
-		this.state = state; 
-	}
-	
-	public int getState() { 
-		return state;
-	}
-	
-	private static final int IDLE = 0, ACCUMULATING = 1, PASSING = 2; 
-	
-	public Accumulator() {
-		accuVic = new Victor(Constants.PORT_ACCUMULATOR_VIC_7);
-	}
-	
-	public void init() {
-		state = IDLE;
-	}
-	
-	public void disable() {
-		state = IDLE;
-	}
-	
-	public void update() {
-		switch(state) {
-			case IDLE:
-				accuVic.set(0.0);
-				break;
-			case ACCUMULATING:
-				accuVic.set(1.0);
-				break;
-			case PASSING:
-				accuVic.set(-1.0);
-				break;
-		}
-	}
-	
-	public static class AccumulateCommand extends RobotCommand {
-		
-		public AccumulateCommand() {
-			subsystemType = RobotCommand.ACCUMULATOR;
-		}
-		
-		public void execute(Subsystem accu) {
-			((Accumulator)accu).setState(ACCUMULATING);
-		}
-	}
-    
-	public static class PassCommand extends RobotCommand {
-		
-		public PassCommand() {
-			subsystemType = RobotCommand.ACCUMULATOR;
-		}
-		
-		public void execute(Subsystem accu) {
-			((Accumulator)accu).setState(PASSING);
-		}
+
+        private Victor accuVic;
+
+        private int state;
+        private double speed;
+
+        public void setState(int state) { 
+                this.state = state; 
+        }
+
+        public int getState() { 
+                return state;
+        }
+
+        private static final int IDLE = 0, ACCUMULATING = 1, PASSING = 2; 
+
+        public Accumulator() {
+                accuVic = new Victor(Constants.PORT_ACCUMULATOR_VIC_7);
+                speed = 0.0;
+        }
+
+        public void init() {
+                state = IDLE;
+        }
+
+        public void disable() {
+                state = IDLE;
+        }
+
+        public void update() {
+                //System.out.println("accuVic: "+accuVic.get());
+                System.out.println(state);
+                switch(state) {
+
+                        case IDLE:
+                                accuVic.set(0.0);
+                                break;
+                        case ACCUMULATING:
+                                accuVic.set(speed);
+                                break;
+                        case PASSING:
+                            System.out.println("PASSING");
+                                accuVic.set(1.0);
+                                break;
+                }
+        }
+
+        public static class AccumulateCommand extends RobotCommand {
+                private double arg;
+                public AccumulateCommand(double speed) {
+                        subsystemType = RobotCommand.ACCUMULATOR;
+                        this.arg = speed;
+                }
+
+                public void execute(Subsystem accu) {
+                        ((Accumulator)accu).speed = arg;
+                        ((Accumulator)accu).setState(ACCUMULATING);
+                }
+        }
+
+        public static class PassCommand extends RobotCommand {
+
+               public PassCommand() {
+                        subsystemType  = RobotCommand.ACCUMULATOR;
+                }
+                public void execute(Subsystem accu) {
+                    System.out.println("");
+                        ((Accumulator)accu).setState(PASSING);
+                }
     }
-    
-	public static class FlushCommand extends RobotCommand {
-		public FlushCommand() {
-			subsystemType  = RobotCommand.ACCUMULATOR;
-		}
-		public void execute(Subsystem accu) {
-			((Accumulator)accu).setState(PASSING);
-		}
-	}
-	
-	public static class SetIdleCommand extends RobotCommand {
-		
-		public SetIdleCommand() {
-			subsystemType = RobotCommand.ACCUMULATOR;
-		}
-		public void execute(Subsystem accu) {
-			((Accumulator)accu).setState(IDLE);
-		}
-	}
-	
-	public static class EjectCommand extends RobotCommand {
-		
-		public EjectCommand() {
-			subsystemType = RobotCommand.ACCUMULATOR;
-		}
-		public void execute(Subsystem accu) {
-			((Accumulator)accu).setState(PASSING);
-		}
-	}
+
+        public static class FlushCommand extends RobotCommand {
+                public FlushCommand() {
+                        subsystemType  = RobotCommand.ACCUMULATOR;
+                }
+                public void execute(Subsystem accu) {
+                        ((Accumulator)accu).setState(PASSING);
+                        System.out.println("Accumulator is Passing");
+                }
+        }
+
+        public static class SetIdleCommand extends RobotCommand {
+
+                public SetIdleCommand() {
+                        subsystemType = RobotCommand.ACCUMULATOR;
+                }
+                public void execute(Subsystem accu) {
+                        ((Accumulator)accu).setState(IDLE);
+                }
+        }
+
+        public static class EjectCommand extends RobotCommand {
+
+                public EjectCommand() {
+                        subsystemType = RobotCommand.ACCUMULATOR;
+                }
+                public void execute(Subsystem accu) {
+                        ((Accumulator)accu).setState(PASSING);
+                }
+        }
 }
